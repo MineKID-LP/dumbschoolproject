f20e262033a952e8f081525e42ac1706d3375917
MineKID_LP <maximilian.schueller@hotmail.com>
2022-04-03 21:54:42 +0200

    Ready up

--- js/musicmanager.js
+++ js/musicmanager.js
@@ -1,64 +1,49 @@
 let sounds = [];
 
 function Sound(tone, timing, duration) {
-  this.tone = tone;
-  this.timing = timing;
-  this.duration = duration;
+     this.tone = tone;
+     this.timing = timing;
+     this.duration = duration;
 }
 
 function addSound(tone, timing, duration) {
-  sounds.push(new Sound(tone, timing, duration));
-  sounds = sortNotes();
+     sounds.push(new Sound(tone, timing, duration));
+     sounds = sortNotes();
 }
 
 function play() {
-  sounds.forEach((item, i) => {
-    playnote(item.tone, item.timing, item.duration);
-  });
+     sounds.forEach((item, i) => {
+          playnote(item.tone, item.timing, item.duration);
+     });
 }
 
-function stop(){
-  return alert("Nein")
+function stop() {
+     return alert("Nein")
 }
 
 function playnote(note, start, end) {
-  return false                                    //GO AWAY
-  var frequency = Note.fromLatin(note).frequency();
-  var context = new AudioContext();
-  var oscillator = context.createOscillator();
-  var gain = context.createGain();
+     return false //GO AWAY
+     var frequency = Note.fromLatin(note).frequency();
+     var context = new AudioContext();
+     var oscillator = context.createOscillator();
+     var gain = context.createGain();
 
-  oscillator.frequency.value = frequency;
+     oscillator.frequency.value = frequency;
 
-  //Types: sine square triangle sawtooth
-  oscillator.type = "sine";
+     //Types: sine square triangle sawtooth
+     oscillator.type = "sine";
 
-  oscillator.connect(gain); //WTF is that shi*
-  gain.connect(context.destination); //What does that mean
+     oscillator.connect(gain); //WTF is that shi*
+     gain.connect(context.destination); //What does that mean
 
-  oscillator.start(start);
-  oscillator.stop(end + start);
+     oscillator.start(start);
+     oscillator.stop(end + start);
 }
 
-function sortNotes() {
-  //bubblesort
-  for (var i = 1; i < sounds.length; i++) {
-    if (sounds[i].timing < sounds[0].timing) {
-      sounds.unshift(sounds.splice(i, 1)[0]);
-    } else if (sounds[i].timing > sounds[i - 1].timing) {
-      continue;
-    } else {
-      for (var j = 1; j < i; j++) {
-        if (
-          sounds[i].timing > sounds[j - 1].timing &&
-          sounds[i].timing < sounds[j].timing
-        ) {
-          sounds.splice(j, 0, sounds.splice(i, 1)[0]);
-        }
-      }
-    }
-  }
-  return sounds;
-}
-
-export { addSound, play, Sound, playnote, stop };
+export {
+     addSound,
+     play,
+     Sound,
+     playnote,
+     stop
+};
