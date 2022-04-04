2c4c8db166ca7d0fcd8276e1ad0194942472f89d
AUDISTat <76568204+JannesKoe@users.noreply.github.com>
2022-04-04 10:00:29 +0200

    Update frontend.js

--- js/frontend.js
+++ js/frontend.js
@@ -1,1 +1,10 @@
-$('#table')
+$('#table').append('<table>');
+
+let rows = 87;
+let allnotes = [];
+
+for(i = 0; i > rows; i++){
+$('table').append('<tr><td>' + output + '</td></tr>';
+}
+
+$('#table').append('</table>');
