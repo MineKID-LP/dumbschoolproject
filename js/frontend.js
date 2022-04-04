ae239664315d4455ca8791e8725f676f9109314c
MineKID_LP <65910050+MineKID-LP@users.noreply.github.com>
2022-04-04 10:20:23 +0200

    FIX IT!!!

--- js/frontend.js
+++ js/frontend.js
@@ -3,6 +3,8 @@ $('#table').append('<table>');
 let rows = 87;
 let allnotes = [];
 
+let output = 'Fucking add your shit when you use it'
+
 for(i = 0; i > rows; i++){
 $('table').append('<tr><td>' + output + '</td></tr>';
 }
