a90e05a12379f1589ddb8c87efaddae1cf7c8b90
maximilian.schueller@hotmail.com <maximilian.schueller@hotmail.com>
2022-04-04 09:16:43 +0200

    Minor Refactor

--- js/main.js
+++ js/main.js
@@ -7,22 +7,3 @@ document.getElementById("play").onclick = function() {
 document.getElementById("stop").onclick = function() {
      MM.stop();
 };
-
-document.getElementById("addnote").onclick = function() {
-     addnote(
-          document.getElementById("note").value,
-          document.getElementById("starttime").value,
-          document.getElementById("duration").value
-     );
-};
-
-function addnote(note, start, duration) {
-     MM.addSound(note, parseFloat(start), parseFloat(duration));
-     let temp = item.cloneNode();
-     temp.innerHTML = item.innerHTML
-          .replaceAll("NOTE", note.toString())
-          .replaceAll("START", start.toString())
-          .replaceAll("DURATION", duration.toString());
-     temp.style.display = "block";
-     wrapper.appendChild(temp);
-}
